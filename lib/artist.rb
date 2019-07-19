f3e8844e2c8a3e8758067a50cfdd03bd20f81c1a
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-19 23:54:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ class Artist
   def add_song(song)
     @songs << song # Add this new song, to the array of song by this artist
     song.artist = self # For the song that was just added, assign this artist as a property
-    @@songs << song
+    @@songs << song # Add the song to the class @@songs array
     song
   end
 
