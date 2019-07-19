7c80eb835f91890261ddbb750cafc58ca3c03616
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-19 22:53:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,8 +16,11 @@ class Artist
 
   def add_song_by_name(name)
     song = Song.new(name)
-    song.artist = self
-    @songs << song
+
+    add_song(song)
+
+    # song.artist = self
+    # @songs << song
   end
 
   def add_song(song)
