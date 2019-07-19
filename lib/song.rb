accd82f0c67f75d2581abda2c20ff0c5e68ad148
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-19 23:56:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,10 @@ class Song
   end
 
   def artist_name
-    if self.artist != nil # self.author.name is too specific. If there is no author object, assigned to the post, then there for sure is no self.author.name
+    if self.artist != nil # self.artist.name is too specific. If there is no author object, assigned to the post, then there for sure is no self.author.name
       self.artist.name # Return the artist name, not the artist object. Artist is the object and name is a property of that object
     else
-      nil # If there is no artist assigned to a 
+      nil # If there is no artist assigned to a
     end
   end
 
